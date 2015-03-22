c48c46c573c015b95da64788ef4576816e218ae4
barabanus <buratin.barabanus@gmail.com>
2015-03-22 18:13:53 +0200

    light control added

--- cactus.ino
+++ cactus.ino
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////
-// Cactus Tracker v1.0.7 / March 9, 2015
+// Cactus Tracker v1.0.9 / March 22, 2015
 // by Maksym Ganenko <buratin.barabanus at Google Mail>
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -7,7 +7,8 @@
 
 const int PIN_LM35    = A4;
 const int PIN_DHT22   = 13;
-const int PIN_HEATER  = 2;
+const int PIN_HEATER  = 10;
+const int PIN_LIGHT   = 2;
 
 const int DELAY_MS    = 1000;
 const int MAGIC       = 10101;
@@ -28,6 +29,7 @@ enum { OFF = 0, ON, AUTO };
 
 int mode              = AUTO;
 bool heater           = false;
+bool light            = false;
 float heaterFrom      = 5.f;
 float heaterTo        = 10.f;
 
@@ -46,20 +48,35 @@ void stopHeater() {
   heater = false;
 }
 
+void startLight() {
+  digitalWrite(PIN_LIGHT, HIGH);
+  light = true;
+}
+
+void stopLight() {
+  digitalWrite(PIN_LIGHT, LOW);
+  light = false;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
 void setup() {
   Serial.begin(115200);
   digitalWrite(PIN_HEATER, LOW);
   pinMode(PIN_HEATER, OUTPUT);
-  
+
+  digitalWrite(PIN_LIGHT, LOW);
+  pinMode(PIN_LIGHT, OUTPUT);
+
   analogReference(INTERNAL);
-  for (int i = 0; i < 10; ++i) {
+  for (int i = 0; i < 100; ++i) {
     analogRead(PIN_LM35);
   }
 }
 
 void loop() {
-   float value = float(analogRead(PIN_LM35)) / 1024 * 1.1 / 10e-3;
-   tempLM35.update(value);
+  float temp = float(analogRead(PIN_LM35)) / 1024 * 1.1 / 10e-3;
+  tempLM35.update(temp);
   
   int code = dhtReader.read22(PIN_DHT22);
   if (code == DHTLIB_OK) {
@@ -67,13 +84,12 @@ void loop() {
     humidityDHT22.update(dhtReader.humidity);    
   }
   
-  if (!tempDHT22.valid()) return;
-  
   if (Serial.available()) {
     if (Serial.parseInt() == MAGIC) {
       int newMode = Serial.parseInt();
       float newHeaterFrom = Serial.parseFloat();
       float newHeaterTo = Serial.parseFloat();
+      int newLight = Serial.parseInt();
       
       if (newMode >= OFF && newMode <= AUTO && newHeaterFrom < newHeaterTo) {
         mode = newMode;
@@ -81,6 +97,9 @@ void loop() {
         heaterTo = newHeaterTo;
         stopHeater();
       }
+
+      if (newLight) startLight();
+      else          stopLight();
     }
   }
   
@@ -95,10 +114,11 @@ void loop() {
   Serial.print("mode = ");            Serial.print(mode);
   Serial.print(", tempLM35 = ");      Serial.print(tempLM35.value);
   Serial.print(", tempDHT22 = ");     Serial.print(tempDHT22.value);
-  Serial.print(", humidity = ");      Serial.print(humidityDHT22.value);
+  Serial.print(", humidityDHT22 = "); Serial.print(humidityDHT22.value);
   Serial.print(", heater = ");        Serial.print(heater);
   Serial.print(", heaterFrom = ");    Serial.print(heaterFrom);
-  Serial.print(", heaterTo = ");      Serial.println(heaterTo);  
+  Serial.print(", heaterTo = ");      Serial.print(heaterTo);
+  Serial.print(", light = ");         Serial.println(light);
   
   delay(DELAY_MS);
 }
--- cactus.py
+++ cactus.py
@@ -1,78 +1,1 @@
-###############################################################################
-# Cactus Tracker v1.0.6 / January 13, 2015
-# by Maksym Ganenko <buratin.barabanus at Google Mail>
-###############################################################################
-
-import serial, re
-import sys, os, traceback
-from datetime import datetime
-
-# arduino serial port in your system
-SERIAL  = (sys.platform == "win32") and "COM5" or "/dev/tty.usbmodem1421"
-
-# input / output files
-INIFILE = "cactusini.txt"
-CMDFILE = "cactuscmd.txt"
-LOGFILE = "cactuslog.txt"
-
-# log update period in seconds
-UPDATE_PERIOD_SEC = 600
-
-###############################################################################
-
-def execute(cmdfile, **argv):
-    if os.path.isfile(cmdfile):
-        try: # input
-            fcmd = open(cmdfile)
-            stream.write(((fcmd.read().strip() + " ") * 10).strip())
-            fcmd.close()
-
-            if "renameTo" in argv:
-                dstfile = argv["renameTo"]
-                if os.path.isfile(dstfile): os.remove(dstfile)
-                os.rename(cmdfile, dstfile)
-        except: traceback.print_exc()
-        if fcmd and not fcmd.closed: fcmd.close()
-
-firstRun = True
-fcmd, flog, timemark, lastState = None, None, None, None
-stream = serial.Serial(SERIAL, 115200)
-
-while True:
-    s = stream.readline()
-    if "mode" in s:
-        record      = dict(re.findall(r"(\w+)\s+=\s+([-.\d]+)", s))
-        mode        = int(record["mode"])
-        tempLM35    = float(record["tempLM35"])
-        tempDHT22   = float(record["tempDHT22"])
-        humidity    = float(record["humidity"])
-        heater      = int(record["heater"])
-        heaterFrom  = float(record["heaterFrom"])
-        heaterTo    = float(record["heaterTo"])
-        state       = (mode, heater, heaterFrom, heaterTo)
-
-        if firstRun:
-            execute(INIFILE)
-            firstRun = False
-
-        execute(CMDFILE, renameTo = INIFILE)
-
-        timeout = not timemark or \
-                 (datetime.now() - timemark).seconds > UPDATE_PERIOD_SEC
-
-        if timeout or state != lastState:
-            output = (datetime.now(), tempLM35, tempDHT22, humidity,
-                      mode, heater, heaterFrom, heaterTo)
-            output = "%s,%.2f,%.2f,%.2f,%d,%d,%.1f,%.1f" % output
-
-            try: # output
-                flog = open(LOGFILE, "a")
-                flog.write(output + "\n")
-            except: traceback.print_exc()
-            if flog: flog.close()
-            print output
-
-            timemark = datetime.now()
-            lastState = state
-
-###############################################################################
+################################################################################ Cactus Tracker v1.0.6 / January 13, 2015# by Maksym Ganenko <buratin.barabanus at Google Mail>###############################################################################import serial, reimport sys, os, tracebackfrom datetime import datetime# arduino serial port in your systemSERIAL  = (sys.platform == "win32") and "COM6" or "/dev/tty.usbmodem1421"# input / output filesINIFILE = "cactusini.txt"CMDFILE = "cactuscmd.txt"LOGFILE = "cactuslog.txt"# log update period in secondsUPDATE_PERIOD_SEC = 600###############################################################################def execute(cmdfile, **argv):    if os.path.isfile(cmdfile):        try: # input            fcmd = open(cmdfile)            stream.write(((fcmd.read().strip() + " ") * 10).strip())            fcmd.close()            if "renameTo" in argv:                dstfile = argv["renameTo"]                if os.path.isfile(dstfile): os.remove(dstfile)                os.rename(cmdfile, dstfile)        except: traceback.print_exc()        if fcmd and not fcmd.closed: fcmd.close()firstRun = Truefcmd, flog, timemark, lastState = None, None, None, Nonestream = serial.Serial(SERIAL, 115200)while True:    s = stream.readline()    if "mode" in s:        record      = dict(re.findall(r"(\w+)\s+=\s+([-.\d]+|nan)", s))        mode        = int(record["mode"])        tempLM35    = float(record["tempLM35"])        tempDHT22   = float(record["tempDHT22"])        humidity    = float(record["humidityDHT22"])        heater      = int(record["heater"])        heaterFrom  = float(record["heaterFrom"])        heaterTo    = float(record["heaterTo"])        light       = int(record["light"])        state       = (mode, heater, heaterFrom, heaterTo, light)        if firstRun:            execute(INIFILE)            firstRun = False        execute(CMDFILE, renameTo = INIFILE)        timeout = not timemark or \                 (datetime.now() - timemark).seconds > UPDATE_PERIOD_SEC        if timeout or state != lastState:            output = (datetime.now(), tempLM35, tempDHT22, humidity,                      mode, heater, heaterFrom, heaterTo, light)            output = "%s,%.2f,%.2f,%.2f,%d,%d,%.1f,%.1f,%d" % output            try: # output                flog = open(LOGFILE, "a")                flog.write(output + "\n")            except: traceback.print_exc()            if flog: flog.close()            print output            timemark = datetime.now()            lastState = state###############################################################################
